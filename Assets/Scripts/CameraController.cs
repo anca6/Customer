f080491dd6cd131044ffc33c598449a140c59cc2
anca6 <526100@student.saxion.nl>
2023-09-19 12:46:02 +0200

    camera switch working
    i still have to make changes to the pos and rotation of the target

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,65 +5,46 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
-    private List<Transform> targetTransforms = new List<Transform>(); // List of target positions & rotations
+    private Transform targetTransform; //so we can take transform from another obj in editor (just drag in the obj to assign)
 
-    [SerializeField]
-    private float transitionDuration = 2.0f; // Transition duration
-
-    private int currentTarget = 0; // Keep track of which target we are at
-    private bool isTransitioning = false;
+    private Vector3 originalPosition; //obj this is attached to original position
+    private Quaternion originalRotation; //obj this is attached to original rotation
+    private bool isToggled = false; //bool to check if we are on first or second camera position
 
     void Start()
     {
-        SetCameraToCurrentTarget(); // Start off with the camera position/rotation of the current object this script is attached to
+        //set originalPosition & originalRotation variable to obj this is attached to like take values
+        originalPosition = transform.position;
+        originalRotation = transform.rotation; 
     }
 
     private void Update()
     {
-        if (isTransitioning)
-            return; // Exit the current method from executing
 
-        if (Input.GetKeyDown(KeyCode.Y))
+        if (Input.GetKeyDown(KeyCode.Y)) //if y is pressed
         {
-            SetNextTarget(); // Go to the next target position/rotation
+            
+            if (isToggled) //toggle between the initial position and the reference transform's position
+            {
+                //go back to the initial position & rotation
+                transform.position = originalPosition;
+                transform.rotation = originalRotation;
+            }
+            else
+            {
+                //move to the target transform position (from the obj)
+                if (targetTransform != null) //check if its not null & actually assigned
+                {
+                    //set to the target position & rotation
+                    transform.position = targetTransform.position;
+                    transform.rotation = targetTransform.rotation;
+                }
+            }
+
+            isToggled = !isToggled; //toggle the state
         }
     }
 
-    private void SetCameraToCurrentTarget()
-    {
-        if (currentTarget >= 0 && currentTarget < targetTransforms.Count)
-        { // Check if currentTarget is a valid number in targetTransforms list
-            transform.position = targetTransforms[currentTarget].position; // Set current position to the object this script is attached to
-            transform.rotation = targetTransforms[currentTarget].rotation; // Set current rotation to the object this script is attached to
-        }
-    }
 
-    private void SetNextTarget()
-    { // Go to the next target position/rotation from the list
-        if (isTransitioning)
-            return; // Exit the current method from executing
 
-        currentTarget = (currentTarget + 1) % targetTransforms.Count; // Calculate the number of the next target, looping around the list if needed
-        StartCoroutine(TransitionCamera(targetTransforms[currentTarget])); // Start transitioning to the next target 
-    }
-
-    IEnumerator TransitionCamera(Transform targetTransform)
-    {
-        isTransitioning = true;
-        float t = 0.0f;
-
-        Vector3 startingPosition = transform.position; // Set start position to current position (the object this script is attached to)
-        Quaternion startingRotation = transform.rotation; // Set start rotation to current rotation (the object this script is attached to)
-
-        while (t < 1.0f)
-        {
-            t += Time.deltaTime / transitionDuration; // Increment t based on time passed and transition duration for smooth interpolation
-
-            transform.position = Vector3.Lerp(startingPosition, targetTransform.position, t); // Move from current position to target position with duration of t
-            transform.rotation = Quaternion.Slerp(startingRotation, targetTransform.rotation, t); // Rotate from current rotation to target rotation with duration of t
-
-            yield return null; // Wait until the next frame before continuing the loop
-        }
-        isTransitioning = false;
-    }
 }
