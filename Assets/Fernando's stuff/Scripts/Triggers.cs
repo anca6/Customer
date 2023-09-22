64f9645009cd79f87a4dc4a50fadba51617da793
anca6 <526100@student.saxion.nl>
2023-09-22 11:55:04 +0200

    added ui pop up
     for everything except extinguisher

--- Assets/Fernando's stuff/Scripts/Triggers.cs
+++ Assets/Fernando's stuff/Scripts/Triggers.cs
@@ -13,9 +13,14 @@ public class Triggers : MonoBehaviour
 
     [SerializeField]
     private Pickup pickup;
-    [SerializeField]
+    
     private OpenWindows window;
+
+    [SerializeField]
     private Interaction interactible;
+
+    [SerializeField]
+    private LockDoor door;
 
     void Start()
     {
@@ -50,6 +55,52 @@ public class Triggers : MonoBehaviour
         if (other.gameObject.CompareTag("Window"))
         {
             interactE.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Alarm"))
+        {
+            if (!interactible.isInteracted)
+            {
+                interactE.SetActive(true);
+            }
+        }
+
+        if (collision.gameObject.CompareTag("Belonging"))
+        {
+                interactE.SetActive(true);
+        }
+
+        if (collision.gameObject.CompareTag("Door"))
+        {
+            interactE.SetActive(true);
+
+            if (door.isClosed)
+            {
+                interactX.SetActive(true);
+            }
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Alarm"))
+        {
+            interactE.SetActive(false); 
+        }
+
+        if (collision.gameObject.CompareTag("Belonging"))
+        {
+            interactE.SetActive(false);
+        }
+
+        if (collision.gameObject.CompareTag("Door"))
+        {
+            interactE.SetActive(false);
+            interactX.SetActive(false);
         }
     }
 
